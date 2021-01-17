36becd8e94b5a4cf723728670c4aa2d465218851
Joseph Beuckman <joebeuckman@gmail.com>
2021-01-17 11:36:54 -0600

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var world = "world";
+function hello(word) {
+    if (word === void 0) { word = world; }
+    return "Hello " + world + "! ";
+}
+exports.hello = hello;
+//# sourceMappingURL=index.js.map
